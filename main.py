705b5d13af22fe8fd9f52028632af2dbe560e5d7
Anthony Sutton <anthony.sutton95@gmail.com>
2024-12-06 15:47:30 -0900

    feat: draws a triangle
    Signed-off-by: Anthony Sutton <anthony.sutton95@gmail.com>

--- main.py
+++ main.py
@@ -1,27 +1,28 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
     pygame.init()
-
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
 
-    running = True
-
-    while running:
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        color = (0, 0, 0)
-        screen.fill(color)
+        screen.fill("black")
+        player.draw(screen)
+        pygame.display.flip()
 
-        pygame.display.flip
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
     main()
+
